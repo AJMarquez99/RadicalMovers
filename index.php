2cda50dbff1264eef4da4d2f1f0dd8b0b7a4bb00
Alejandro Marquez <AlejandroM@bloomstoday.local>
2019-01-09 15:47:15 -0500

    Finished structure of the site.

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
   include ('components/_header.html');
   include ('components/_intro.html');
-
+  include ('components/_services.html');
+  include ('components/_estimate.html');
+  include ('components/_testimonials.html');
+  include ('components/_our-work.html');
   include ('components/_footer.html');
 ?>
