3d2cb504270765921ae2f7a902664191d36b1920
Alejandro Marquez <AlejandroM@bloomstoday.local>
2019-02-08 17:50:15 -0500

    Start form validation

--- js/scripts.js
+++ js/scripts.js
@@ -223,6 +223,48 @@ function slideServices() {
   $cell.find('.js-collapser').closeSummary;
 }
 
+//Form Validation
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function formValidation() {
+  let inputBlock = $(this);
+  let inputName = $(this).attr("id");
+  switch (inputName) {
+    case "name":
+      if ( !inputBlock.val() ) {
+        inputBlock[0].style.border = "2px solid red";
+        inputBlock[0].style.boxShadow = "0px 0px 5px red";
+      } else {
+        inputBlock[0].style.textTransform = "Capitalize";
+        inputBlock[0].style.border = "2px solid #008000";
+        inputBlock[0].style.boxShadow = "0px 0px 5px #009900";
+      }
+      break;
+    case "email":
+      if ( !inputBlock.val() || !validateEmail( inputBlock.val() )  ) {
+        inputBlock[0].style.border = "2px solid red";
+        inputBlock[0].style.boxShadow = "0px 0px 5px red";
+      } else {
+        inputBlock[0].style.border = "2px solid #008000";
+        inputBlock[0].style.boxShadow = "0px 0px 5px #009900";
+      }
+      break;
+    case "phone_number":
+      if ( !inputBlock.val() ) {
+        inputBlock[0].style.border = "2px solid red";
+        inputBlock[0].style.boxShadow = "0px 0px 5px red";
+      } else {
+        inputBlock[0].style.border = "2px solid #008000";
+        inputBlock[0].style.boxShadow = "0px 0px 5px #009900";
+      }
+      break;
+  }
+}
+
+$("input").blur(formValidation);
 $(".arrow").click(slideServices);
 $(window).one("load",stickyScroll);
 $(window).scroll(stickyScroll);
