057596df1d6df7a6938a2a9ef34b6f49dd23d063
Alejandro Marquez <alejandromarquez@live.com>
2019-02-14 20:38:46 -0500

    (Alejandro Marquez - H) Cleaning up merge

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
   include ('components/_main.html');
-
-<<<<<<< HEAD
-=======
-  include ('components/_main.html');
   include ('components/_about.html');
 
->>>>>>> cff5055b41e1b7778bd72c90ea38a1c51f282d2e
   include ('components/_footer.html');
 ?>
