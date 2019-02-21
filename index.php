31961b68dc77f7d5ea919681060cd73ee93e8d6d
Alejandro Marquez <alejandromarquez@live.com>
2019-02-21 01:45:09 -0500

    (AMarquez - H) Finishing Structure for add ons

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
   include ('components/_main.html');
   include ('components/_about.html');
-
+  include ('components/_services.html');
+  include ('components/_pricing.html');
+  include ('components/_tips.html');
   include ('components/_footer.html');
 ?>
---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+function sectionCall() {
+  let pageLink = this.getAttribute("data-block");
+  let page;
+  switch ( pageLink.slice(8) ) {
+    case "about":
+      findOtherSections();
+      sectionRise(pageLink);
+      break;
+    case "services":
+      findOtherSections();
+      sectionRise(pageLink);
+      break;
+    case "pricing":
+      findOtherSections();
+      sectionRise(pageLink);
+      break;
+    case "tips":
+      findOtherSections();
+      sectionRise(pageLink);
+      break;
+    default:
+      findOtherSections();
+      break;
+  }
+}
+
+function sectionRise(id) {
+  let page = document.getElementById(id);
+  page.classList.add("active-page");
+}
+
+function sectionLower(id) {
+  let page = document.getElementById(id);
+  page.classList.remove("active-page");
+}
+
+function findOtherSections() {
+  for (let otherPage of $(".nav-tab") ) {
+    let otherPageLink = otherPage.getAttribute("data-block");
+    sectionLower(otherPageLink);
+  }
+}
+
+$(".nav-tab").click(sectionCall);
+$(".nav-logo").click( function() {
+  findOtherSections();
+});
