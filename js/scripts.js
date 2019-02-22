71e302eea77333eed46e951c31e31c2bb3b71052
Alejandro Marquez <AlejandroM@bloomstoday.local>
2019-02-22 17:04:05 -0500

    (AMarquez - W) Added Image Gallery

--- js/scripts.js
+++ js/scripts.js
@@ -41,7 +41,63 @@ function findOtherSections() {
   }
 }
 
+function createGalleryArrows(arrowDirection) {
+  let arrow = document.createElement("button");
+  let direction = document.createTextNode(arrowDirection);
+  arrow.append(direction);
+  return arrow;
+}
+
+function createGallery() {
+  let galleryContainer = $("#gallery_container");
+  let rightArrow = createGalleryArrows("›");
+  let leftArrow = createGalleryArrows("‹");
+  galleryContainer.append(rightArrow);
+  galleryContainer.append(leftArrow);
+
+  rightArrow.classList.add("next");
+  leftArrow.classList.add("prev");
+
+  $('.prev').click(function() {
+    plusSlides(-1);
+  });
+  $('.next').click(function() {
+    plusSlides(1);
+  });
+
+  for ( let galleryImage of $(".mySlides") ) {
+    let imgBlock = document.createElement("img");
+    let imgSrc = galleryImage.getAttribute("data-img-src");
+    imgBlock.setAttribute("src", imgSrc);
+    galleryImage.append(imgBlock);
+  }
+}
+
+//Gallery Controls
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = $(".mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slides[slideIndex-1].classList.add("active");
+}
+
+$(".mySlides").ready(createGallery);
+
 $(".nav-tab").click(sectionCall);
+
 $(".nav-logo").click( function() {
   findOtherSections();
 });
@@ -49,6 +105,7 @@ $(".nav-logo").click( function() {
 $("aside.header-info").on("mouseover", function() {
   this.classList.add("aside-hover");
 });
+
 $("aside.header-info").on("mouseout", function() {
   this.classList.remove("aside-hover");
 });
