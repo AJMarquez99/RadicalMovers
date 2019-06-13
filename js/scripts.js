16b0d5056078dd5f8ad0fb518be0d5a6b1b50279
Alejandro Marquez <alejandromarquez@live.com>
2019-06-12 20:21:39 -0400

    (Alejandro H) Adding Finishing Touches

--- js/scripts.js
+++ js/scripts.js
@@ -230,6 +230,20 @@ function buttonActive() {
 	}
 }
 
+var acc = document.getElementsByClassName("service-accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
 /*function closeNavClick() {
 		let mobileLinks = $("#mobile_nav").find("a");
 		for (let link of mobileLinks) {
